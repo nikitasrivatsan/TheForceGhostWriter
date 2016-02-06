3d602200bd507c931c82fc672d55fab316d95ab5
asrivat1 <akshay.srivatsan@gmail.com>
2016-02-06 18:14:22 +0000

    LSTM is training

--- lstm.py
+++ lstm.py
@@ -3,12 +3,14 @@
 from tensorflow.models.rnn import rnn_cell
 import tensorflow as tf
 import sys
+import math
 import numpy as np
 
+# hyperparameters
 BATCH_SIZE = 20
 HIDDEN_SIZE = 200
-EPOCHS = 10
-NUM_STEPS = 200
+EPOCHS = 5
+NUM_STEPS = 100
 
 # data manipulation
 data = open("star_wars_vii.txt", 'r').read()
@@ -32,18 +34,20 @@ def main():
     W_soft = tf.Variable(tf.truncated_normal([HIDDEN_SIZE, num_chars], stddev = 0.01))
     b_soft = tf.Variable(tf.constant(0.01, shape = [num_chars]))
 
-    loss = tf.Variable(tf.constant(0.01))
+    loss = tf.Variable(tf.constant(0.0))
     for i in range(0, NUM_STEPS):
         with tf.variable_scope("LSTM" + str(i)):
             output, state = cell(tf.reshape(words[:, i], [BATCH_SIZE, 1]), state)
             prediction = tf.nn.softmax(tf.matmul(output, W_soft) + b_soft)
 
             for j in range(0, BATCH_SIZE):
-                tf.add(loss, tf.slice(prediction, tf.pack([j, target_words[j, i]]), [1, 1]))
+                loss = tf.add(loss, tf.log(tf.slice(prediction, tf.pack([j, target_words[j, i]]), [1, 1])))
 
-    loss /= BATCH_SIZE * NUM_STEPS
-
+    loss = - tf.truediv(loss, float(NUM_STEPS) / float(BATCH_SIZE))
     final_state = state
+
+    # define train step
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
     print "Segmenting input data"
 
@@ -60,21 +64,42 @@ def main():
     # initialize everything
     sess.run(tf.initialize_all_variables())
 
-    print "Iterating over batches"
+    print "Training model"
+
+    # saving the model
+    saver = tf.train.Saver()
+    sess.run(tf.initialize_all_variables())
+    checkpoint = tf.train.get_checkpoint_state("saved_networks")
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print "Successfully loaded:", checkpoint.model_checkpoint_path
+    else:
+        print "Could not find old network weights"
     
     # iterate over batches
-    current_state = initial_state.eval()
-    total_loss = 0.0
-    for i in range(0, epoch_size):
-        x = data_batched[:, i * NUM_STEPS : (i + 1) * NUM_STEPS]
-        y = data_batched[:, i * NUM_STEPS + 1 : (i + 1) * NUM_STEPS + 1]
+    for e in range(0, EPOCHS):
+        current_state = initial_state.eval()
+        total_loss = 0.0
 
-        current_state, current_loss = sess.run([final_state, loss],
-                                               feed_dict = {initial_state : current_state, words : x, target_words : y})
+        for i in range(0, epoch_size):
+            x = data_batched[:, i * NUM_STEPS : (i + 1) * NUM_STEPS]
+            y = data_batched[:, i * NUM_STEPS + 1 : (i + 1) * NUM_STEPS + 1]
 
-        total_loss += current_loss
+            train_step.run(feed_dict = {initial_state : current_state,
+                                        words : x,
+                                        target_words : y})
 
-    print total_loss
+            current_state, current_loss = sess.run([final_state, loss],
+                                                   feed_dict = {initial_state : current_state, words : x, target_words : y})
+
+            total_loss += current_loss[0][0]
+
+        # save weights
+        saver.saver(sess, "saved_networks/", global_step = e)
+
+        total_loss /= epoch_size
+        print "Average loss per sequence for epoch", e, ": ", total_loss
+
 
 if __name__ == "__main__":
     main()
